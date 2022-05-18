c1e7904c3728af79dd59e49e97dc9314e5c25f37
전지혜 <jeonjh9535@naver.com>
2022-05-18 13:24:51 +0900

    ViewPager 프로젝트 생성

---
+++ app/src/main/java/com/asianaidt/viewpager/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.asianaidt.viewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // ViewPager : (가로)스크롤뷰 + 스크롤 위치를 강제시키는 기능이 추가되어있는 클래스
+    }
+}
